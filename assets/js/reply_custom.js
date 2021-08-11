13c9ff93600076ac7d2d8da8f8668c84d58b80ef
heera.ahn <hi.heeraahn@gmail.com>
2021-08-11 17:34:23 +0900

    덧글 스타일변경 테스트3

--- assets/js/reply_custom.js
+++ assets/js/reply_custom.js
@@ -1,7 +1,7 @@
 window.onload = function(){
   const _iframe = document.getElementById("utterances-comments").getElementsByTagName("iframe")[0];
 
-  _iframe.addEventListener('load', e => { 
+  _iframe.onload = function(){
     alert("연결");
     let cs1 = document.querySelector("h1.timeline-header");
     cs1.style.paddingLeft = "0";
