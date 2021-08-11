fbed73ad1fa72a2be2b051f8110716c99635f0c8
heera.ahn <hi.heeraahn@gmail.com>
2021-08-11 18:59:03 +0900

    promise TypeError 해결중

--- assets/js/reply_custom.js
+++ assets/js/reply_custom.js
@@ -2,6 +2,7 @@ window.$ = window.jQuery = require('jquery');
 
 var _iframe = $('.utterances-comments iframe');
 $(_iframe).on("load", function() {
+  event.preventDefault();
   let head = $(_iframe).contents().find("head");
   let css = '<style>.timeline-header{ padding-left : 0; }</style>';
   $(head).append(css);
