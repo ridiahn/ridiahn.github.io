e9c719b5c6c5eb39585f15c8fbd7c1d3ac0b7d07
heera.ahn <hi.heeraahn@gmail.com>
2021-08-11 16:32:03 +0900

    iframe js style 변경 테스트2

--- assets/js/reply_custom.js
+++ assets/js/reply_custom.js
@@ -1,7 +1,7 @@
 window.onload = function(){
   // alert("연결성공");
   //cs=coustomstyle element
-  let cs1 = document.querySelector( ".timeline-header>a");
-  cs1.style.paddingLeft = "0"
+  let cs1 = document.querySelector(".timeline-header");
+  cs1.style.paddingLeft = "0";
   
 }
