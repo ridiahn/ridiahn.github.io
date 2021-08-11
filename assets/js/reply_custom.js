b557e5673116fa9702b816f48e0355ae0059af01
heera.ahn <hi.heeraahn@gmail.com>
2021-08-11 19:27:45 +0900

    헤더영역 스타일 수정

--- assets/js/reply_custom.js
+++ assets/js/reply_custom.js
@@ -1,8 +1,9 @@
-window.$ = window.jQuery = require('jquery');
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
 
 var _iframe = $('.utterances-comments iframe');
-$(_iframe).on("load", function() {
-  event.preventDefault();
+$(_iframe).on("load", function(e) {
+  e.preventDefault();
   let head = $(_iframe).contents().find("head");
   let css = '<style>.timeline-header{ padding-left : 0; }</style>';
   $(head).append(css);
