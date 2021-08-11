5ab7ea4dbe09fe3d45ebd6d3be675899d88ffe8c
heera.ahn <hi.heeraahn@gmail.com>
2021-08-11 17:23:41 +0900

    덧글 스타일변경 테스트2

--- assets/js/reply_custom.js
+++ assets/js/reply_custom.js
@@ -1,8 +1,8 @@
 window.onload = function(){
-  // alert("연결성공");
-  const _iframe = document.getElementsByClassName('utterances-frame')[0];
+  const _iframe = document.getElementById("utterances-comments").getElementsByTagName("iframe")[0];
 
-  _iframe.addEventListener('load', e => {  
+  _iframe.addEventListener('load', e => { 
+    alert("연결");
     let cs1 = document.querySelector("h1.timeline-header");
     cs1.style.paddingLeft = "0";
   })
