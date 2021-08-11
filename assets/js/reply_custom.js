a2f1a54436363ea00d4b07ce68111c2a99ccb62f
heera.ahn <hi.heeraahn@gmail.com>
2021-08-11 17:00:27 +0900

    제이쿼리삽입

--- assets/js/reply_custom.js
+++ assets/js/reply_custom.js
@@ -1,5 +1,4 @@
-@import './vendor/jquery/jquery-3.5.1'
-
+<script src="./vendor/jquery/jquery-3.5.1"></script>
 $("iframe").one("load", function() {
   $("iframe").contents().find("h1.timeline-header").css( "padding-left", "0");
 })
