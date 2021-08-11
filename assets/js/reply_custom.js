b455c435f158a4cd3b5ae5fb24002f6743c1a0fd
heera.ahn <hi.heeraahn@gmail.com>
2021-08-11 17:14:27 +0900

    덧글 스타일변경 시도중 선택자 에러수정

--- assets/js/reply_custom.js
+++ assets/js/reply_custom.js
@@ -1,8 +1,8 @@
 window.onload = function(){
   // alert("연결성공");
   const _iframe = document.getElementsByClassName('utterances-frame')[0];
-  
-  iframe1.addEventListener('load', e => {  
+
+  _iframe.addEventListener('load', e => {  
     let cs1 = document.querySelector("h1.timeline-header");
     cs1.style.paddingLeft = "0";
   })
