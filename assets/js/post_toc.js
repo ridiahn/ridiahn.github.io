2745b2d25ab977238e37b1852180d5f1ba0aefb6
heera.ahn <hi.heeraahn@gmail.com>
2021-08-17 00:53:45 +0900

    toc content가 끝나는 지점에서 멈추도록 위치변경

--- assets/js/post_toc.js
+++ assets/js/post_toc.js
@@ -1,8 +1,8 @@
 /* _variables.scss -> $medium: 768px;*/
 let bp = 768;
 let rightSide = document.getElementsByClassName("sidebar__right")[0];
-let _page = document.getElementsByClassName("page")[0];
-let pageHeader = document.getElementsByClassName("page__inner-wrap")[0].getElementsByTagName("header")[0];
+let page_inner =  document.getElementsByClassName("page__inner-wrap")[0];
+let pageHeader = page_inner.getElementsByTagName("header")[0];
 
 window.onload = function(){
   postioningToc();
@@ -19,6 +19,6 @@ function postioningToc(){
     pageHeader.append(rightSide);
   } else {
     /*desktop screen*/
-    _page.after(rightSide);
+    page_inner.append(rightSide);
   }
 }
