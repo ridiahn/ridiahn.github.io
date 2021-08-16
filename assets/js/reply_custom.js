6ad591a93ef37a0bde88e300520762b38f997b19
heera.ahn <hi.heeraahn@gmail.com>
2021-08-16 15:28:06 +0900

    utterances comment UI 접근시도포기, 코드 제거

--- assets/js/reply_custom.js
+++
@@ -1,9 +0,0 @@
-var $ = window.jQuery;
-
-var _iframe = $('.utterances-comments iframe');
-$(_iframe).on("load", function(e) {
-  e.preventDefault();
-  let head = $(_iframe).contents().find("head");
-  let css = '<style>.timeline-header{ padding-left : 0; }</style>';
-  $(head).append(css);
-});
