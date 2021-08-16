56298979bb8294bd2b848718d882a3ffe556d6a2
heera.ahn <hi.heeraahn@gmail.com>
2021-08-16 14:58:45 +0900

    toc스크립트 원상복구 및 수정일자 없을시 표기스타일 수정

--- assets/js/post_toc.js
+++ assets/js/post_toc.js
@@ -3,8 +3,6 @@ let bp = 768;
 let rightSide = document.getElementsByClassName("sidebar__right")[0];
 let _page = document.getElementsByClassName("page")[0];
 let pageHeader = document.getElementsByClassName("page__inner-wrap")[0].getElementsByTagName("header")[0];
-var screenWidth = window.innerWidth;
-
 
 window.onload = function(){
   postioningToc();
@@ -15,6 +13,7 @@ window.onresize = function(){
 };
 
 function postioningToc(){
+  var screenWidth = window.innerWidth;
   if( screenWidth <= bp ){
     /*tablet screen*/
     pageHeader.append(rightSide);
